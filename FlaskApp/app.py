e887949e5de92e0a89ad95503e5effd0febe3c6a
wgzesg <wgzesg@gmail.com>
2020-10-17 14:45:20 +0800

    add remote link

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -10,14 +10,7 @@ app.register_blueprint(view)
 
 
 # Config
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://{username}:{password}@{host}:{port}/{database}"\
-    .format(
-        username="postgres",
-        password="Wgzesg",
-        host="localhost",
-        port=5432,
-        database="postgres"
-    )
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://rymjgqmiucferj:8e07c2a21509d15003f8cd12113219d602a59b1f6298da579e9e375b32da2c4c@ec2-3-231-16-122.compute-1.amazonaws.com:5432/df67v1kmbj2l9t?sslmode=require"
 app.config["SECRET_KEY"] = "A random key to use flask extensions that require encryption"
 
 # Initialize other components
