3508d7978a08b9b405968c3e331a0c5aac8890fa
wgzesg <wgzesg@gmail.com>
2020-10-18 22:05:28 +0800

    hide url

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -13,7 +13,6 @@ app.register_blueprint(view)
 # Config
 DATABASE_URL = os.environ['DATABASE_URL']
 
-#app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://stiwcbueymtugr:37c18745f46467dbb42518378b2e794e8a5941d1febe535b3997daf14507e6cb@ec2-54-156-53-71.compute-1.amazonaws.com:5432/dekba31jf7kr6f?sslmode=require"
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL+"?sslmode=require"
 app.config["SECRET_KEY"] = "A random key to use flask extensions that require encryption"
 
