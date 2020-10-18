97724c2101f569ac63122c1bbf435889ff475a23
wgzesg <wgzesg@gmail.com>
2020-10-18 22:03:45 +0800

    try hide url

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 from __init__ import db, login_manager
@@ -10,7 +11,10 @@ app.register_blueprint(view)
 
 
 # Config
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://stiwcbueymtugr:37c18745f46467dbb42518378b2e794e8a5941d1febe535b3997daf14507e6cb@ec2-54-156-53-71.compute-1.amazonaws.com:5432/dekba31jf7kr6f?sslmode=require"
+DATABASE_URL = os.environ['DATABASE_URL']
+
+#app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://stiwcbueymtugr:37c18745f46467dbb42518378b2e794e8a5941d1febe535b3997daf14507e6cb@ec2-54-156-53-71.compute-1.amazonaws.com:5432/dekba31jf7kr6f?sslmode=require"
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL+"?sslmode=require"
 app.config["SECRET_KEY"] = "A random key to use flask extensions that require encryption"
 
 # Initialize other components
