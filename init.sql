a20f014f0d4959e088d516462754d302d03f49d3
zsk612 <zhangsk612@gmail.com>
2020-10-22 22:48:25 +0800

    update sql

--- init.sql
+++ init.sql
@@ -41,6 +41,15 @@ CREATE TABLE users (
     postalcode INTEGER
 );
 
+CREATE TABLE role (
+    usertype VARCHAR PRIMARY KEY NOT NULL 
+);
+
+CREATE TABLE userroles (
+    contact VARCHAR PRIMARY KEY NOT NULL REFERENCES public.users(contact),
+    usertype VARCHAR NOT NULL REFERENCES public.role(usertype)
+);
+
 CREATE TABLE pets(
     petname VARCHAR NOT NULL,
     pcontact VARCHAR NOT NULL REFERENCES public.users(contact),
@@ -57,7 +66,7 @@ CREATE TABLE available (
 );
 
 CREATE TABLE cantakecare (
-    ccontact VARCHAR NOT NULL REFERENCES public.careuserstakers(contact),
+    ccontact VARCHAR NOT NULL REFERENCES public.users(contact),
     category VARCHAR REFERENCES public.categories(category),
     dailyprice INT NOT NULL,
     PRIMARY KEY (ccontact, category)
