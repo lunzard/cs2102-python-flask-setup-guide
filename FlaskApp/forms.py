3ef3a0cad6788937167206aef5fcd62730dc4acd
zsk612 <zhangsk612@gmail.com>
2020-10-22 21:50:38 +0800

    Delete admin petowner and caretaker import

--- FlaskApp/forms.py
+++ FlaskApp/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, DateField
 from wtforms.validators import InputRequired, ValidationError, EqualTo
-from models import Admins, Petowners, Caretakers
+from models import Users
 from datetime import date
 
 def is_valid_name(form, field):
@@ -9,9 +9,7 @@ def is_valid_name(form, field):
         raise ValidationError('This field should only contain alphabets')
 
 def is_valid_contact(self, contact):
-    contact = ((Admins.query.filter_by(contact=contact.data).first()) or
-                (Petowners.query.filter_by(contact=contact.data).first()) or
-                (Caretakers.query.filter_by(contact=contact.data).first()))
+    contact = (Users.query.filter_by(contact=contact.data).first())
     if contact:
         raise ValidationError('That contact is already being registered. Please choose a different one.')
         
