32b040431e9c66ae5dc61ea1d571a49d40d7db22
CFZeon <e0406826@u.nus.edu>
2020-10-25 16:07:39 +0800

    update table

--- FlaskApp/tables.py
+++ FlaskApp/tables.py
@@ -1,16 +1,16 @@
 from flask_table import Table, Col, ButtonCol
 
 class userInfoTable(Table):
-    username = Col('Username')
-    contact = Col('Contact')
-    card = Col('Card')
-    usertype = Col('User Type')
+    username = Col('Username ')
+    contact = Col('Contact ')
+    card = Col('Card ')
+    usertype = Col('User Type ')
     postalcode = Col('Postal Code')
 
 class editPetTable(Table):
-    petname = Col('Pet Name')
-    pcontact = Col('Contact')
-    category = Col('Pet Name')
-    age = Col('Pet Name')
-    edit = ButtonCol('Edit', 'view.render_owner_pet_update', url_kwargs=dict(petname='petname', category='category', age='age'))
+    petname = Col('Pet Name ')
+    pcontact = Col('Contact ')
+    category = Col('Pet Name ')
+    age = Col('Age ')
+    edit = ButtonCol('Edit ', 'view.render_owner_pet_update', url_kwargs=dict(petname='petname', category='category', age='age'))
     delete = ButtonCol('Delete', 'view.render_owner_pet_delete', url_kwargs=dict(petname='petname'))
