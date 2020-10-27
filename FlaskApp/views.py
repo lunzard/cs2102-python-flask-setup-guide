4bdf36eab8a9b28e9f27b993028f95cb5e241bab
wgzesg <wgzesg@gmail.com>
2020-10-27 21:14:52 +0800

    update col names

--- FlaskApp/views.py
+++ FlaskApp/views.py
@@ -256,11 +256,11 @@ def render_caretaker_available_edit():
     ac = current_user.contact
     astart = request.args.get('startday')
     aend = request.args.get('endday')
-    available = Available.query.filter_by(startdate=astart,enddate=aend,ccontact=ac).first()
+    available = Available.query.filter_by(startday=astart,endday=aend,ccontact=ac).first()
     if available:
         form = AvailableUpdateForm(obj=available)
         if request.method == 'POST' and form.validate_on_submit():
-            thisavailable = Available.query.filter_by(startdate=astart,enddate=aend,ccontact=ac).first()
+            thisavailable = Available.query.filter_by(startday=astart,endday=aend,ccontact=ac).first()
             thisavailable.startday = form.startdate.data
             thisavailable.endday = form.enddate.data
             db.session.commit()
@@ -274,7 +274,7 @@ def render_caretaker_available_delete():
     ac = current_user.contact
     astart = request.args.get('startday')
     aend = request.args.get('endday')
-    available = Available.query.filter_by(startdate=astart,enddate=aend,ccontact=ac).first()
+    available = Available.query.filter_by(startday=astart,endday=aend,ccontact=ac).first()
     if available:
         if request.method == 'POST':
             db.seesion.delete(available)
